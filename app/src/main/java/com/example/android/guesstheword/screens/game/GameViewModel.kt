4cc5b71c4ecd3899e1e07d42fcc21c3e6b8136bc
NiradaSukchit <59160104@go.buu.ac.th>
2019-10-02 22:38:48 +0700

    Encapsulate the LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,21 +1,30 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    // The current word
-    val word = MutableLiveData<String>()
-    // The current score
-    val score = MutableLiveData<Int>()
 
-    // The list of words - the front of the list is the next word to guess
+    // The current _word
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    // The current _score
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+
+
+    // The list of words - the front of the list is the next _word to guess
     private lateinit var wordList: MutableList<String>
     init {
         Log.i("GameViewModel", "GameViewModel created!")
-        word.value = ""
-        score.value = 0
+        _word.value = ""
+        _score.value = 0
     }
 
     /**
@@ -54,26 +63,26 @@ class GameViewModel : ViewModel() {
     }
 
     /**
-     * Moves to the next word in the list
+     * Moves to the next _word in the list
      */
     private fun nextWord() {
         if (!wordList.isEmpty()) {
-            //Select and remove a word from the list
-            word.value = wordList.removeAt(0)
+            //Select and remove a _word from the list
+            _word.value = wordList.removeAt(0)
         }
 //        updateWordText()
 //        updateScoreText()
     }
     fun onSkip() {
         if (!wordList.isEmpty()) {
-            score.value = (score.value)?.minus(1)
+            _score.value = (_score.value)?.minus(1)
         }
         nextWord()
     }
 
      fun onCorrect() {
         if (!wordList.isEmpty()) {
-            score.value = (score.value)?.plus(1)
+            _score.value = (_score.value)?.plus(1)
         }
         nextWord()
     }
